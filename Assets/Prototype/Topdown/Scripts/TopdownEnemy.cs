2214e0a1d159421f34f57d463b65fb485fceedeb
Atuan <jain60501@gmail.com.tw>
2023-11-29 13:29:35 +0800

    feat enemy movement will not overlap

--- Assets/Prototype/Topdown/Scripts/TopdownEnemy.cs
+++ Assets/Prototype/Topdown/Scripts/TopdownEnemy.cs
@@ -5,8 +5,8 @@ namespace Prototype.Topdown{
 	public class TopdownEnemy : MonoBehaviour{
 		[SerializeField] private int hp = 2;
 		[SerializeField] private int moveSpeed = 3;
-		[HideInInspector] public Transform playerTransform;
 		private Rigidbody2D _rigidbody;
+		public Action<TopdownEnemy> OnEnemyGetKill;
 
 
 		private void Start(){
@@ -16,17 +16,14 @@ namespace Prototype.Topdown{
 
 		public void Hit(){
 			hp--;
-			if(hp <= 0) Destroy(gameObject);
+			_rigidbody.AddForce(-_rigidbody.velocity, ForceMode2D.Impulse);
+			if(hp > 0) return;
+			OnEnemyGetKill?.Invoke(this);
 		}
 
-		private void Update(){
-			Movement();
-		}
-
-		private void Movement(){
-			Vector2 direction = (playerTransform.position - transform.position).normalized;
+		public void FollowingTarget(Vector2 position){
+			var direction = (position - (Vector2)transform.position).normalized;
 			var movePosition = _rigidbody.position + direction * (moveSpeed * Time.fixedDeltaTime);
-
 			_rigidbody.MovePosition(movePosition);
 		}
 	}
--- Assets/Prototype/Topdown/Scripts/TopdownEnemyRoot.cs
+++ Assets/Prototype/Topdown/Scripts/TopdownEnemyRoot.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Prototype.Topdown{
 	public class TopdownEnemyRoot : MonoBehaviour{
 		[SerializeField] private GameObject enemyPrefab;
 		[SerializeField] private Transform player;
 		[SerializeField] private float spawnDuration = 1f;
+		[SerializeField] private float spawnRadius = 5f;
+		[SerializeField] private float randomTargetRadius = 0.5f;
 
+		[HideInInspector] public List<TopdownEnemy> enemies = new();
+		private readonly List<Vector2> _targetPositionList = new();
 		private ColdDownTimer _timer;
 
 		private void Start(){
@@ -14,20 +20,62 @@ namespace Prototype.Topdown{
 		}
 
 		private void FixedUpdate(){
+			SetEnemyFollowingTarget();
 			if(!_timer.CanInvoke()) return;
 			SpawnEnemy();
 			_timer.Reset();
 		}
 
+		private void SetEnemyFollowingTarget(){
+			for(var i = 0; i < enemies.Count; i++){
+				var enemy = enemies[i];
+				enemy.FollowingTarget(GetTargetPositionByIndex(i));
+			}
+		}
+
+		private Vector2 GetTargetPositionByIndex(int index){
+			if(_targetPositionList.Count - 1 >= index && _targetPositionList[index] != Vector2.zero)
+				return _targetPositionList[index] + (Vector2)player.position;
+			while(_targetPositionList.Count - 1 < index){
+				_targetPositionList.Add(Vector2.zero);
+			}
+
+			var randomPosition = Random.insideUnitCircle * randomTargetRadius;
+			while(_targetPositionList.Any(x => Vector2.Distance(randomPosition, x) < 0.3f)){
+				randomPosition = Random.insideUnitCircle * randomTargetRadius;
+			}
+
+			_targetPositionList[index] = randomPosition;
+			return _targetPositionList[index] + (Vector2)player.position;
+		}
+
 		private void SpawnEnemy(){
-			var enemyClone = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
-			enemyClone.GetComponent<TopdownEnemy>().playerTransform = player;
+			var enemyClone = Instantiate(enemyPrefab, GetSpawnPosition(), Quaternion.identity);
+			var topdownEnemy = enemyClone.GetComponent<TopdownEnemy>();
+			topdownEnemy.OnEnemyGetKill += OnEnemyGetKill;
+			enemies.Add(topdownEnemy);
+		}
+
+		private void OnEnemyGetKill(TopdownEnemy enemy){
+			enemies.Remove(enemy);
+			Destroy(enemy.gameObject);
 		}
 
 
 		private void OnDrawGizmos(){
 			Gizmos.color = Color.red;
-			Gizmos.DrawWireCube(transform.position, Vector2.one * 0.5f);
+			Gizmos.DrawWireSphere(player.position, spawnRadius);
+			Gizmos.color = Color.green;
+			Gizmos.DrawWireSphere(player.position, randomTargetRadius);
+			foreach(var target in _targetPositionList){
+				Gizmos.DrawWireCube(target + (Vector2)player.position, Vector3.one * 0.3f);
+			}
+		}
+
+		private Vector2 GetSpawnPosition(){
+			Vector2 center = player.position;
+			var randomPosition = Random.insideUnitCircle.normalized * spawnRadius;
+			return center + randomPosition;
 		}
 	}
 }
